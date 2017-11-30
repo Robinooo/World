31427cf6b325ae6c7c8814f732da329b604aae44
martindeg <martindeguise@gmail.com>
2017-11-30 09:54:17 +0100

    change for player to use the dialog bow for code

--- WOR/src/wor/Player.java
+++ WOR/src/wor/Player.java
@@ -1,6 +1,7 @@
 package wor;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * This class represents the player, who can move, interact with characters and
@@ -11,8 +12,6 @@ import javax.swing.*;
  */
 public class Player extends Character {
 
-    
-    
     //private int PersuasionBar = 200;//persuasion default value is always set on 100
     private NoteBook note; // the player's notebook
     private Room currentRoom; // the room where the player is currently
@@ -20,10 +19,10 @@ public class Player extends Character {
     private int persuasionBar; // the number of persuasion points of the player
     private int lifeBar; // the player's number of points of life
     private int timeBar; // the player's remaining time (a number of minutes)
+    private JFrame frame; // The frame for the code dialog box
+    private JLabel label;
+    private Icon icon;
     
-    
-    
-
     /**
      * Constructor for objects of class Player
      */
@@ -108,26 +107,25 @@ public class Player extends Character {
 //            //perdu
 //        }
 //    }
-
     /**
-     * This method allows the player to take an item that is present in a room, add it to his 
-     * inventory and update his number of persuasion points by adding the points associated to 
-     * the item he is picking up
+     * This method allows the player to take an item that is present in a room,
+     * add it to his inventory and update his number of persuasion points by
+     * adding the points associated to the item he is picking up
      */
     public void takeItem(JTextArea zoneTexte) {
         zoneTexte.setText("");
         if (currentRoom.listItem.size() > 0) {
             for (int i = 0; i < currentRoom.listItem.size(); i++) {
                 //inv.addObject(currentRoom.listItem.get(i));
-                 inv.addItems(currentRoom.getItem(i));
-                 //setPersuasion2(currentRoom.getItem(i).getPersupoints()); // add the persuasion points associated with the item to the player
-                 setPersuasion(currentRoom.getItem(i).getPersupoints()); // add the persuasion points associated with the item to the player
+                inv.addItems(currentRoom.getItem(i));
+                //setPersuasion2(currentRoom.getItem(i).getPersupoints()); // add the persuasion points associated with the item to the player
+                setPersuasion(currentRoom.getItem(i).getPersupoints()); // add the persuasion points associated with the item to the player
 
-                 zoneTexte.append(currentRoom.getItem(i).getDescription() + "\n ");
-                 note.addText(currentRoom.getItem(i).getDescription());
-                 currentRoom.listItem.remove(i);
+                zoneTexte.append(currentRoom.getItem(i).getDescription() + "\n ");
+                note.addText(currentRoom.getItem(i).getDescription());
+                currentRoom.listItem.remove(i);
 //                 if (i > 0) {
-                     i--;
+                i--;
 //                 }
             }
             inv.displayItems(inv.ItemsList);
@@ -141,7 +139,7 @@ public class Player extends Character {
      * @param direction : north, east... Represents the direction of the
      * movement of the player
      */
-     public void move(String direction) {
+    public void move(String direction) {
 
         Room nextRoom = null;
         Room testRoom = null; // Room to try the door of a room 
@@ -152,36 +150,39 @@ public class Player extends Character {
             if (testRoom.getDoor() != null) {
                 if (testRoom.getDoor().isOpenable() == true) { //if isOpenable -> true if is openable
                     nextRoom = currentRoom.getRoom(direction);
-                } 
-                else {
+                } else {
                     if (testRoom.getDoor().getHaveCodeLock()) {
                         if (!inv.ItemsList.isEmpty()) {
-                        for (int i = 0; i < inv.ItemsList.size(); i++) {
-                            boolean test = testRoom.getDoor().openDoorPass(inv.ItemsList.get(i).getName());
-                            if (test) { // Try each object from the inventory -> return true if the door is unlock
-                                testRoom.getDoor().setOpenable(true);
-                                nextRoom = currentRoom.getRoom(direction);         
-                                setTime(2);
+                            for (int i = 0; i < inv.ItemsList.size(); i++) {
+                                boolean test = testRoom.getDoor().openDoorPass(inv.ItemsList.get(i).getName());
+                                if (test) { // Try each object from the inventory -> return true if the door is unlock
+                                    testRoom.getDoor().setOpenable(true);
+                                    nextRoom = currentRoom.getRoom(direction);
+                                    setTime(2);
 
-                            } else {
-                                nextRoom = currentRoom;
+                                } else {
+                                    nextRoom = currentRoom;
+                                }
                             }
-                        }} else { nextRoom = currentRoom; }
-                    }
-                    else {
+                        } else {
+                            nextRoom = currentRoom;
+                        }
+                    } else {
                         if (!inv.ItemsList.isEmpty()) {
-                        for (int i = 0; i < inv.ItemsList.size(); i++) {
-                            boolean test = testRoom.getDoor().openDoorKey(inv.ItemsList.get(i).getName());
-                            if (test) {
-                                testRoom.getDoor().setOpenable(true);
-                                nextRoom = currentRoom.getRoom(direction);
-                                setTime(2);
-                                break;
-                            } 
-                            else {
-                                nextRoom = currentRoom;
+                            for (int i = 0; i < inv.ItemsList.size(); i++) {
+                                boolean test = testRoom.getDoor().openDoorKey(inv.ItemsList.get(i).getName());
+                                if (test) {
+                                    testRoom.getDoor().setOpenable(true);
+                                    nextRoom = currentRoom.getRoom(direction);
+                                    setTime(2);
+                                    break;
+                                } else {
+                                    nextRoom = currentRoom;
+                                }
                             }
-                        }} else { nextRoom = currentRoom; }
+                        } else {
+                            nextRoom = currentRoom;
+                        }
                     }
                 }
             } else // there is no door
@@ -194,64 +195,57 @@ public class Player extends Character {
         currentRoom = nextRoom;
     }
 
-   
-
     public Room getCurrentRoom() {
         return currentRoom;
     }
-     
-     public void explore(JTextArea zoneTexte){
-         
-         
-         if(currentRoom.listItem.size() > 0 ){
-             zoneTexte.setText(currentRoom.listItem.size()+" items in the room.");
-                for(int i=0; i < currentRoom.listItem.size(); i++) 
-                {
-                    if (currentRoom.listItem.get(i).getHidden()) {
+
+    public void explore(JTextArea zoneTexte) {
+
+        if (currentRoom.listItem.size() > 0) {
+            zoneTexte.setText(currentRoom.listItem.size() + " items in the room.");
+            for (int i = 0; i < currentRoom.listItem.size(); i++) {
+                if (currentRoom.listItem.get(i).getHidden()) {
                     //setTime(10);
                     setTime(4);
 
-                    }
-                    else {
+                } else {
                     //setTime(5);
                     setTime(2);
 
-                    }
                 }
-            } else {                    
-             //setTime(5);
-             setTime(2);
+            }
+        } else {
+            //setTime(5);
+            setTime(2);
 
-             zoneTexte.setText("There is no items here, you have lost precious time exploring for nothing \n");
-         }
-    // on perd 5 minutes de temps des qu'on explore si il y pas d'item ou si un item n'est pas caché 
-    // on perds 10 minutes si un item est caché
-    
-                           
-    
+            zoneTexte.setText("There is no items here, you have lost precious time exploring for nothing \n");
         }
+        // on perd 5 minutes de temps des qu'on explore si il y pas d'item ou si un item n'est pas caché 
+        // on perds 10 minutes si un item est caché
+
+    }
 
     public void speak(JTextArea zoneTexte) {
         if (currentRoom.getTalkingInTheRoom() == null) {
             setTime(2);
             zoneTexte.setText("There is no one to talk to here, you think about talking to yourself and loose 5 minutes \n of your precious time \n");
-           
+
         } else {
-             //setTime(5);
-             setTime(2);
-             setPersuasion(5);
-            zoneTexte.setText(currentRoom.getTalkingInTheRoom().talk(persuasionBar)  + "\n");
+            //setTime(5);
+            setTime(2);
+            setPersuasion(5);
+            zoneTexte.setText(currentRoom.getTalkingInTheRoom().talk(persuasionBar) + "\n");
         }
-        
+
     }
- 
-    
-    
-    public int getPersuasion(){
+
+    public int getPersuasion() {
         return persuasionBar;
     }
+
     /**
      * setter for the Persuasion attribute
+     *
      * @param k Persuasion value between 0 and 100
      */
 //    public void setPersuasion(int k){
@@ -259,55 +253,46 @@ public class Player extends Character {
 //        PersuasionBar=k;
 //        }
 //    }
-    
-    
+
     public void setPersuasion(Integer ppersua) {
         persuasionBar = persuasionBar + ppersua;
     }
-    
+
     /*
     * This method allow to add 10 points to the Persuasion bar of the player
-    */   
-
+     */
     /**
      *
      */
-   
-    public void add10Persuasion()
-    {
-        if(persuasionBar >= 90)
-        {
+    public void add10Persuasion() {
+        if (persuasionBar >= 90) {
             setPersuasion(100);
+        } else {
+            setPersuasion(persuasionBar + 10);
         }
-        else setPersuasion(persuasionBar+10);
     }
-   
+
     /*
     * This method allow to delete 10 points to the Persuasion bar of the player
-    */
-
+     */
     /**
      *
      */
-
-    public void del10Persuasion()
-    {
-        if(persuasionBar <= 10)
-        {
+    public void del10Persuasion() {
+        if (persuasionBar <= 10) {
             setPersuasion(0);
+        } else {
+            setPersuasion(persuasionBar - 10);
         }
-        else setPersuasion(persuasionBar-10);
     }
 
-
-      
-
-
-     public int getLife(){
+    public int getLife() {
         return lifeBar;
     }
+
     /**
      * setter for the Persuasion attribute
+     *
      * @param k Persuasion value between 0 and 100
      */
 //    public void setPersuasion(int k){
@@ -315,19 +300,18 @@ public class Player extends Character {
 //        PersuasionBar=k;
 //        }
 //    }
-    
-    
+
     public void setLife(Integer plife) {
         lifeBar = lifeBar - plife;
     }
-    
-    
-    
-     public int getTime(){
+
+    public int getTime() {
         return timeBar;
     }
+
     /**
      * setter for the Persuasion attribute
+     *
      * @param k Persuasion value between 0 and 100
      */
 //    public void setPersuasion(int k){
@@ -335,12 +319,34 @@ public class Player extends Character {
 //        PersuasionBar=k;
 //        }
 //    }
-    
-    
+
     public void setTime(Integer ptime) {
         timeBar = timeBar - ptime;
     }
-   
-   
     
-} 
+    /** Sets the text displayed at the bottom of the frame. */
+    void setLabel(String newText) {
+        label.setText(newText);
+    }
+
+
+    private String dialogCodeInput() {
+        String s = (String) JOptionPane.showInputDialog(
+                frame,
+                "Enter your code:\n",
+                "Code Dialog",
+                JOptionPane.PLAIN_MESSAGE,
+                icon,
+                null,
+                "");
+
+//If a string was returned, say so.
+        if ((s != null) && (s.length() > 0)) {
+            return s;
+        }
+
+//If you're here, the return value was null/empty.
+        setLabel("Come on, finish the sentence!");
+        return s;
+    }
+}
